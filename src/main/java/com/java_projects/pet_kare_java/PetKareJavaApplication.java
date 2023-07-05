242129d48ee7146a3b07408811f63f090761fba3
Rihan soares <rihan145@gmail.com>
2023-07-05 16:01:05 -0300

    first commit

---
+++ src/main/java/com/java_projects/pet_kare_java/PetKareJavaApplication.java
@@ -0,0 +1,13 @@
+package com.java_projects.pet_kare_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetkareJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetKareJavaApplication.class, args);
+	}
+
+}
